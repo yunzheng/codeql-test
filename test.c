6b64e790ee5a1c234b4050a109046fcb5d296514
Yun Zheng Hu <hu@fox-it.com>
2022-11-09 11:36:37 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int codeql_test(char *name)
+{
+    uint16_t u;
+    uint32_t hash = strlen(name);
+    for (u = 0; u < strlen(name); u++) {
+        hash += name[u];
+    }
+    return 0;
+}
+
+int main(){
+	printf("hello world\n");
+}
